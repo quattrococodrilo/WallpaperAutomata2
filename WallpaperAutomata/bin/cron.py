eb8039ee8600a7a1dc3c3fc04570301346cc41b6
Fernando Cruz <quattrococodrilo@gmail.com>
2022-02-11 06:04:19 -0600

    Avances

--- WallpaperAutomata/bin/cron.py
+++ WallpaperAutomata/bin/cron.py
@@ -1,12 +1,48 @@
-from os import system
+from pathlib import Path
+from random import choice
 from time import sleep
 
+from WallpaperAutomata.Vendors.Pexels import Pexels
+from WallpaperAutomata.Wallpapper.Config import Config
+from WallpaperAutomata.Wallpapper.Paper import Paper
+from WallpaperAutomata.Wallpapper.SaveImage import SaveImage
 
-def exec():
+
+def papper():
+    filePath = (Path(__file__)
+                .parent
+                .parent
+                .parent
+                .joinpath('data/config.yml'))
+
+    config = Config.create(filePath).data
+
+    pexelsData = config['vendors']['Pexels']
+
+    pexels = Pexels.create(pexelsData['token'], pexelsData['query'])
+
+    photos = pexels.photos()
+
+    photo = choice(photos)
+
+    photo_name = photo['url'].split('/')[-1]
+
+    store_path = (Path(config['config']['store'])
+                  .joinpath(photo_name))
+
+    savei = SaveImage.create(store_path)
+    image = savei.save(photo['url'])
+
+    print(image)
+
+    Paper.create(image)
+
+
+def loop():
     while True:
-        system('python3 -m unittest tests.Wallpaper.PaperTest')
-        sleep(60 * 30)
+        papper()
+        sleep(60*30)
 
 
 if __name__ == '__main__':
-    exec()
+    loop()
